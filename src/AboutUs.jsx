ce760e769de0aa8791b1a4d152ddd2fc2a56586e
Onne164 <57313631+Onne164@users.noreply.github.com>
2024-08-31 14:07:10 +0300

    added introduction about the company

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import './AboutUs.css';
+import React from "react";
+import "./AboutUs.css";
 
 function AboutUs() {
   return (
     <div className="about-us-container">
-      {/* <h1 className="about-us-heading">About Us</h1> */}
-      <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity!</p>
+      <h1 className="about-us-heading">Welcome to e-Plant Herbaticum</h1> *
+      {/*   <p className="about-us-description">Unlock Nature’s Secrets for Health and Beauty!</p> */}
       <p className="about-us-content">
-        At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of 
-        high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and 
-        more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every 
-        plant enthusiast.
+        Our online store is your trusted source for high-quality herbal
+        products, tea blends, and natural beauty essentials designed to help you
+        achieve balance and well-being in your daily life.
       </p>
       {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
       <p className="about-us-content">
-        Our team of experts is dedicated to ensuring that each plant meets our strict standards of quality and care. 
-        Whether you're a seasoned gardener or just starting your green journey, we're here to support you every step of 
-        the way. Feel free to explore our collection, ask questions, and let us help you find the perfect plant for your 
-        home or office.
+        Our products come from reputable suppliers and undergo thorough quality
+        checks to ensure only the best for your health and well-being. We use
+        only natural ingredients to ensure an authentic and effective
+        experience.
       </p>
       {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
-
       <p className="about-us-content">
-        Join us in our mission to create a greener, healthier world. Visit Paradise Nursery today and experience the 
-        beauty of nature right at your doorstep.
+        Explore our product range and start your journey to natural wellness
+        today!
       </p>
     </div>
   );
